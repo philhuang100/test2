c75aec9257aafbaf1cddfffe062aaca51a5001a4
philhuang100 <phil.huang100@gmail.com>
2016-07-25 09:50:07 +0800

    第一次提交

---
+++ MainActivity.java
@@ -0,0 +1,23 @@
+package tw.com.pcschool.t072202;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv = (WebView) findViewById(R.id.webView);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setSupportZoom(true);
+        wv.getSettings().setBuiltInZoomControls(true);
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl("http://tw.yahoo.com");
+    }
+}
